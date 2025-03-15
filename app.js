dc3db7355022ee2b724f94ac17d66be3ccf3d6cb
Mehu Kumar Sahu <mehul.www.mk@gmail.com>
2025-03-15 11:06:24 +0530

    ms

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const path = require('path');
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set(express())
+
+app.get('/', (req, res) => {
+  res.render('./user/home')
+})
+
+app.listen(port, () => {
+    console.log(`Server started on: http://localhost:${port}`);
+});
