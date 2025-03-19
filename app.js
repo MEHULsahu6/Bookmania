f3073adcd40eaa94a1abeddd9221748afca389de
jay <jaykumarsoni944@gmail.com>
2025-03-19 12:37:34 +0530

    home page 2

--- app.js
+++ app.js
@@ -31,30 +31,14 @@ app.get('/Explore', (req, res) => {
 app.get('/cart', (req, res) => {
   res.render('./user/cart')
 })
-
-app.get('/orders', (req, res) => {
-  const orders = [
-     { id: 1, date: '2023-10-01', total: 29.99, status: 'Delivered' },
-     { id: 2, date: '2023-10-05', total: 15.99, status: 'Pending' },
-  ];
-  res.render('./user/orders', { orders });
-});
-
 app.get('/wishlist', (req, res) => {
-  const wishlist = [
-     { image: '/img/book-1.png', title: 'Book 1', discountPrice: 11.99, originalPrice: 19.99 },
-     { image: '/img/book-2.png', title: 'Book 2', discountPrice: 11.99, originalPrice: 19.99 },
-  ];
-  res.render('./user/whishlist', { wishlist });
-});
+  res.render('./user/wishlist')
+})
 
-app.get('/explore', (req, res) => {
-  const books = [
-     { id: 1, image: '/img/book-1.png', title: 'Book 1', discountPrice: 7.99, originalPrice: 14.99 },
-     { id: 2, image: '/img/book-2.png', title: 'Book 2', discountPrice: 7.99, originalPrice: 14.99 },
-  ];
-  res.render('./user/explore', { books });
-});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server started on: http://localhost:${port}`);
 });
