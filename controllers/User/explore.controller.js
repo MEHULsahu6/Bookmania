089b9b4da598784d8b6e719cf43c98fa4d1b4af0
Mehu Kumar Sahu <mehul.www.mk@gmail.com>
2025-04-05 16:34:56 +0530

    ms

--- controllers/User/explore.controller.js
+++ controllers/User/explore.controller.js
@@ -1,3 +1,11 @@
-exports.explore = (req, res) => {
-    res.render('User/explore');
+const Book = require('../../models/book.model');
+
+exports.explore = async (req, res) => {
+    try {
+        const books = await Book.find(); // Fetch all books
+        res.render('User/explore', { books }); // Pass books to the view
+    } catch (error) {
+        console.error('Error loading books:', error);
+        res.status(500).send('Server Error');
+    }
 };
