9a6a33afbb3b8d1707adcadccf004d964a269868
jay <jaykumarsoni944@gmail.com>
2025-04-06 22:20:09 +0530

    Update .env file

--- controllers/User/cart.controller.js
+++ controllers/User/cart.controller.js
@@ -1,7 +1,6 @@
 const Cart = require('../../models/cart.model');
 const Book = require('../../models/book.model');
 const User = require('../../models/user.model');
-
 exports.cart = async (req, res) => {
     try {
         const cart = await Cart.findOne({ user: req.user._id })
