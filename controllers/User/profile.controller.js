e2b73bdaf4f10e7c5e4faca1f6b4f6a77b58d565
jay <jaykumarsoni944@gmail.com>
2025-04-07 19:52:31 +0530

    Update .env file

--- controllers/User/profile.controller.js
+++ controllers/User/profile.controller.js
@@ -3,11 +3,71 @@ const User = require('../../models/user.model');
 
 exports.profile = async (req, res) => {
     try {
-        const userProfile = await UserProfile.findOne({ user: req.user._id })
-            .populate('user')
+        const userProfile = await UserProfile.findOne({ user: req.user.id })
+            .populate('user', '-password')  // Exclude password
             .populate('orderHistory');
-        res.render('User/profile', { userProfile });
+            
+        if (!userProfile) {
+            return res.status(404).render('User/profile', { error: 'Profile not found' });
+        }
+
+        res.render('User/profile', { 
+            userProfile,
+            user: userProfile.user,
+            error: null 
+        });
     } catch (error) {
+        console.error('Profile Error:', error);
         res.status(500).render('User/profile', { error: 'Error loading profile' });
+    }
+};
+
+exports.updatePhone = async (req, res) => {
+    try {
+        const { phoneNumber } = req.body;
+        let userProfile = await UserProfile.findOne({ user: req.user.id });
+        
+        if (!userProfile) {
+            // Create new profile if it doesn't exist
+            userProfile = new UserProfile({
+                user: req.user.id,
+                fullName: req.user.name,
+                email: req.user.email,
+                phoneNumber
+            });
+        } else {
+            userProfile.phoneNumber = phoneNumber;
+        }
+        
+        await userProfile.save();
+        res.redirect('/profile');
+    } catch (error) {
+        console.error('Update Phone Error:', error);
+        res.status(500).json({ error: 'Error updating phone number' });
+    }
+};
+
+exports.updateGender = async (req, res) => {
+    try {
+        const { gender } = req.body;
+        let userProfile = await UserProfile.findOne({ user: req.user.id });
+        
+        if (!userProfile) {
+            // Create new profile if it doesn't exist
+            userProfile = new UserProfile({
+                user: req.user.id,
+                fullName: req.user.name,
+                email: req.user.email,
+                gender
+            });
+        } else {
+            userProfile.gender = gender;
+        }
+        
+        await userProfile.save();
+        res.redirect('/profile');
+    } catch (error) {
+        console.error('Update Gender Error:', error);
+        res.status(500).json({ error: 'Error updating gender' });
     }
 };
--- routes/user/profile.routes.js
+++ routes/user/profile.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const profileController = require('../../controllers/User/profile.controller');
 const { jwtMiddleware } = require('../../middlewares/JWTauth');
 
-
-router.get('/',jwtMiddleware, profileController.profile);
+router.get('/', jwtMiddleware, profileController.profile);
+// Add these new routes
+router.post('/update-profile/phone', jwtMiddleware, profileController.updatePhone);
+router.post('/update-profile/gender', jwtMiddleware, profileController.updateGender);
 
 module.exports = router;
