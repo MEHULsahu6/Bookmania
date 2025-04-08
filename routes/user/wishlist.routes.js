3ff6f2bf9d86ec069466513ad3fbf71873579eae
jay <jaykumarsoni944@gmail.com>
2025-04-08 14:55:14 +0530

    Update .env file

--- routes/user/wishlist.routes.js
+++ routes/user/wishlist.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const wishlistController = require('../../controllers/User/wishlist.controller');
 const { jwtMiddleware } = require('../../middlewares/JWTauth');
 
-router.get('/',jwtMiddleware, wishlistController.wishlist);
+router.get('/', jwtMiddleware, wishlistController.wishlist);
+router.post('/toggle', jwtMiddleware, wishlistController.toggleWishlist);
+router.delete('/remove/:bookId', jwtMiddleware, wishlistController.removeFromWishlist);
 
 module.exports = router;
